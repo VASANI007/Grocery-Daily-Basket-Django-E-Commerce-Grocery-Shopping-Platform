853204cf6f473dce8505c6ad559aa5381b0c5631
Daksh vasani <131647682+VASANI007@users.noreply.github.com>
2026-06-26 08:05:17 +0530

    Add files via upload

---
+++ daily-basket/dd/static/js/app.js
@@ -0,0 +1,37 @@
+const sign_in_btn = document.querySelector("#sign-in-btn");
+const sign_up_btn = document.querySelector("#sign-up-btn");
+const container = document.querySelector(".container");
+
+sign_up_btn.addEventListener('click', () =>{
+    container.classList.add("sign-up-mode");
+});
+
+sign_in_btn.addEventListener('click', () =>{
+    container.classList.remove("sign-up-mode");
+});
+
+
+function openModal() {
+    document.getElementById("passwordModal").style.display = "block";
+    }
+
+function closeModal() {
+    document.getElementById("passwordModal").style.display = "none";
+    }
+
+    function openTab(evt, tabName) {
+    let i, tabcontent, tablinks;
+    
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+    
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].classList.remove("active");
+    }
+
+    document.getElementById(tabName).style.display = "block";
+    evt.currentTarget.classList.add("active");
+    }
